3c1f514cc3d8b995a24d664eb8d527733067a728
Javier Antiles <javier_susy_nike@yahoo.com>
2018-01-19 22:37:08 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -19,15 +19,9 @@ def won?(board)
 
 
   WIN_COMBINATIONS.each do |win_array|
-    win_combination=["","",""]
-
     win_index_1 = win_array[0]
     win_index_2 = win_array[1]
     win_index_3 = win_array[2]
-
-    win_combination = win_array[0]
-    win_combination = win_array[1]
-    win_combination = win_array[3]
 
     position_1 = board[win_index_1]
     position_2 = board[win_index_2]
