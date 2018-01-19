2ac0c03d2a260719f01060b84629997a5c22085c
Javier Antiles <javier_susy_nike@yahoo.com>
2018-01-19 22:37:50 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -21,6 +21,8 @@ def won?(board)
     win_index_2 = win_array[1]
     win_index_3 = win_array[2]
 
+    win_combination << win_index_1
+
     position_1 = board[win_index_1]
     position_2 = board[win_index_2]
     position_3 = board[win_index_3]
