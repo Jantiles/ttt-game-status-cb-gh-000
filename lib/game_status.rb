470f433bcfd8f128409d8d4cfcf786778dd24c0c
Javier Antiles <javier_susy_nike@yahoo.com>
2018-01-19 22:39:38 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -32,8 +32,7 @@ def won?(board)
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
       return win_combination
     else
-      win_combination.clear
-      return false
+      
     end
   end
 end
